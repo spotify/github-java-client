d358965d5af9ebbd729886371b1afa7ae311a2c5
Henrique <henriquet@spotify.com>
2020-04-24 10:21:59 +0200

    Add StatusEvent properties (#11)
    * Add StatusEvent properties
    
    * Add license

--- src/test/java/com/spotify/github/v3/activity/events/StatusEventTest.java
+++ src/test/java/com/spotify/github/v3/activity/events/StatusEventTest.java
@@ -1,6 +1,6 @@
 /*-
  * -\-\-
- * github-api
+ * github-client
  * --
  * Copyright (C) 2016 - 2020 Spotify AB
  * --
@@ -27,20 +27,18 @@ import static org.hamcrest.core.Is.is;
 
 import com.google.common.io.Resources;
 import com.spotify.github.jackson.Json;
-import com.spotify.github.v3.prs.PullRequestActionState;
 import java.io.IOException;
 import org.junit.Test;
 
-public class PullRequestEventTest {
-
+public class StatusEventTest {
   @Test
   public void testDeserialization() throws IOException {
     String fixture =
         Resources.toString(
-            getResource(this.getClass(), "fixtures/pull_request_event.json"), defaultCharset());
-    final PullRequestEvent prEvent = Json.create().fromJson(fixture, PullRequestEvent.class);
-    assertThat(prEvent.action(), is(PullRequestActionState.OPENED));
-    assertThat(prEvent.number(), is(1));
-    assertThat(prEvent.eventInstallation().id(), is(1));
+            getResource(this.getClass(), "fixtures/status_event.json"), defaultCharset());
+    final StatusEvent statusEvent = Json.create().fromJson(fixture, StatusEvent.class);
+    assertThat(statusEvent.context(), is("default"));
+    assertThat(statusEvent.sha(), is("9049f1265b7d61be4a8904a9a27120d2064dab3b"));
+    assertThat(statusEvent.state(), is("success"));
   }
 }
