c7c15963bf9f748cceac121e0762a626703735b7
Henrique Truta <henriquet@spotify.com>
2020-04-27 11:49:42 +0200

    update header

--- src/test/java/com/spotify/github/v3/prs/PullRequestTest.java
+++ src/test/java/com/spotify/github/v3/prs/PullRequestTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -64,7 +64,7 @@ public class PullRequestTest {
   }
 
   @Test
-  public void testDeserializationMergeParams() throws IOException {
+  public void testDeserializationMergeParamsOmitsFields() throws IOException {
     final MergeParameters params = ImmutableMergeParameters.builder()
         .commitMessage("a message")
         .sha("6dcb09b5b57875f334f61aebed695e2e4193db5e")
