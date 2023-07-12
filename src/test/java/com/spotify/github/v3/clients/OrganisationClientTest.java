b40ca623066749591cbd546fc7d1cc14f541391a
Ellie Kelsch <elliek@spotify.com>
2023-07-12 14:05:04 +0100

    feat: add test to test team client creation

---
+++ src/test/java/com/spotify/github/v3/clients/OrganisationClientTest.java
@@ -0,0 +1,69 @@
+/*-
+ * -\-\-
+ * github-api
+ * --
+ * Copyright (C) 2016 - 2020 Spotify AB
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -/-/-
+ */
+
+package com.spotify.github.v3.clients;
+
+import static com.google.common.io.Resources.getResource;
+import static java.nio.charset.Charset.defaultCharset;
+import static java.util.concurrent.CompletableFuture.completedFuture;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.google.common.io.Resources;
+import com.spotify.github.jackson.Json;
+import com.spotify.github.v3.Team;
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import org.junit.Before;
+import org.junit.Test;
+
+public class OrganisationClientTest {
+
+  private GitHubClient github;
+
+  private OrganisationClient organisationClient;
+
+  private Json json;
+
+  private static String getFixture(String resource) throws IOException {
+    return Resources.toString(getResource(TeamClientTest.class, resource), defaultCharset());
+  }
+
+  @Before
+  public void setUp() {
+    github = mock(GitHubClient.class);
+    organisationClient = new OrganisationClient(github, "github");
+    json = Json.create();
+    when(github.json()).thenReturn(json);
+  }
+
+  @Test
+  public void testTeamClient() throws Exception {
+    final TeamClient teamClient = organisationClient.createTeamClient(github, "github");
+    final CompletableFuture<Team> fixture =
+        completedFuture(json.fromJson(getFixture("team_get.json"), Team.class));
+    when(github.request("/orgs/github/teams/justice-league", Team.class)).thenReturn(fixture);
+    final Team team = teamClient.getTeam("justice-league").get();
+    assertThat(team.id(), is(1));
+    assertThat(team.name(), is("Justice League"));
+  }
+}
