7f12a4a05188a292e9df872247434f9d7b323663
Ajmal Kottilingal <ajmal.kottilingal@transferwise.com>
2024-05-09 00:45:06 +0100

    remove unused import
    Signed-off-by: Ajmal Kottilingal <ajmal.kottilingal@transferwise.com>

--- src/test/java/com/spotify/github/v3/clients/RepositoryClientTest.java
+++ src/test/java/com/spotify/github/v3/clients/RepositoryClientTest.java
@@ -77,7 +77,6 @@ import okhttp3.Protocol;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
-import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
